b0ee847b7be926c098c566ccc36c95b799318a19
Cory Bass <cory.bass.07@gmail.com>
2020-11-12 12:53:00 -0600

    updated formatting of items.py

--- items.py
+++ items.py
@@ -5,7 +5,7 @@ class Item:
         self.value = value
 
     def __str__(self):
-        return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+        return f"{self.name}\n=====\n{self.description}\nValue: {self.value}\n"
 
 
 class Gold(Item):
@@ -13,7 +13,7 @@ class Gold(Item):
         self.amt = amt
         super().__init__(
             name="Gold",
-            description="A round coin with {} stamped on the front.".format(str(self.amt)),
+            description=f"A round coin with {str(self.amt)} stamped on the front.",
             value=self.amt)
 
 
@@ -23,7 +23,7 @@ class Weapon(Item):
         super().__init__(name, description, value)
 
     def __str__(self):
-        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
+        return f"{self.name}\n=====\n{self.description}\nValue: {self.value}\nDamage: {self.damage}"
 
 
 class Rock(Weapon):
